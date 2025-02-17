e00ab2cfe26c1119c51b49b76da2b4b2a7e1c0b5
wezee-emori <wisdom.emori@gmail.com>
2025-02-17 09:21:21 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
-<script setup lang="ts">
-import Todos from './components/Todos.vue'
-</script>
+import React, { useState } from "react";
 
-<template>
-  <main>
-    <Todos />
-  </main>
-</template>
+function App() {
+  const [task, setTask] = useState("");
+  const [message, setMessage] = useState("");
+
+  const addTask = async () => {
+    const res = await fetch(https://dpc1ccf9hc.execute-api.us-east-1.amazonaws.com, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id: Date.now().toString(), task })
+    });
+    const data = await res.json();
+    setMessage(data.message);
+  };
+
+  return (
+    <div>
+      <h1>Serverless To-Do List</h1>
+      <input value={task} onChange={(e) => setTask(e.target.value)} />
+      <button onClick={addTask}>Add Task</button>
+      {message && <p>{message}</p>}
+    </div>
+  );
+}
+
+export default App;
+
 
