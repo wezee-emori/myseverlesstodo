e3b7f70ad4c338bba77b6dc5856fdc9cbd134296
wezee-emori <wisdom.emori@gmail.com>
2025-02-17 17:29:43 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
-<script setup lang="ts">
-import Todos from './components/Todos.vue'
-</script>
+import React, { useEffect, useState } from "react";
 
-<template>
-  <main>
-    <Todos />
-  </main>
-</template>
+function App() {
+  const [scores, setScores] = useState([]);
+  const ws = new WebSocket(wss://v7xr2g4fgh.execute-api.us-east-1.amazonaws.com/production/);
 
+  useEffect(() => {
+    ws.onmessage = (event) => {
+      setScores(JSON.parse(event.data));
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Live Scores</h1>
+      {scores.map((match) => (
+        <p key={match.match_id}>{match.score}</p>
+      ))}
+    </div>
+  );
+}
+
+export default App;
